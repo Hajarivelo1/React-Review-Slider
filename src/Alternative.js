1f5dd27bccf7e24ba8ede77bf393c310e5a08249
Hajarivelo1 <93727504+Hajarivelo1@users.noreply.github.com>
2023-03-17 11:59:00 +0300

    Premier Commit

---
+++ src/Alternative.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alternative = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Alternative
---
+++ src/Alternative2.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Alternative2 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Alternative2
